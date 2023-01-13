26ee74213650a9e7d9e3818f11cd2cf5c2427f3e
Richard Meyer <richie.p.meyer@gmail.com>
2023-01-13 14:12:04 -0600

    update

--- prepare.py
+++ prepare.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 import sklearn.preprocessing
 import pandas as pd
 import numpy as np
+import wrangle
 
 
 # In[2]:
@@ -31,16 +32,19 @@ def scale_minmax(train,validate,test):
     Takes in train, validate, and test sets and returns the minmax scaled dfs
     '''
     scaler = sklearn.preprocessing.MinMaxScaler()
-    
-    train[train.columns] = scaler.fit_transform(train[train.columns])
-    validate[validate.columns] = scaler.fit_transform(validate[validate.columns])
-    test[test.columns] = scaler.fit_transform(test[test.columns])
+    scaler.fit(train)
+    train[train.columns] = scaler.transform(train[train.columns])
+    validate[validate.columns] = scaler.transform(validate[validate.columns])
+    test[test.columns] = scaler.fit(test[test.columns])
     
     return train, validate, test
 
 
 # In[ ]:
 
-
+def get_split():
+    df = wrangle.wrangle_zillow()
+    train, validate, test = split_data(df)
+    return train, validate, test
 
 
